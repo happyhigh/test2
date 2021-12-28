90de70826acec9e0557feb1e74b78d0d87cf1793
happyhigh <dongboaka@gmail.com>
2021-12-28 17:51:27 +0900

    init3

--- kp.py
+++ kp.py
@@ -27,7 +27,6 @@ class MonitoringKP(threading.Thread):
     def run(self):   
         binance = ccxt.binance()
         while True:
-            time.sleep(self.cycle)
             debug('kimchi premium monitoring', 1)
 
             # recv binance price
@@ -56,15 +55,32 @@ class MonitoringKP(threading.Thread):
             message += 'o max kp[%s] : %s\n'%(max_token, round(max_values, 3))
             message += 'o min kp[%s] : %s\n'%(min_token, round(min_values, 3))
             message += 'o cal kp : %s\n'%(round(max_values-min_values, 3))
-            print(message)
+            debug(message, 2)
             send_message(message)
+
+            time.sleep(self.cycle)
+
     
 
 def find_key(dict, val):
         return next(key for key, value in dict.items() if value == val)
 
-
 # hedge monitoring
+class MonitoringHedge(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.cycle = 60 * 2
+        # self.cycle = 5
+
+    def run(self):   
+        binance = ccxt.binance()
+        while True:
+            debug('binance hedge monitoring', 1)
+            send_message('binance hedge monitoring')
+            time.sleep(self.cycle)
+
+
+
 def future_monitoring():
     pass
 
@@ -81,7 +97,8 @@ def debug(msg, level):
         if level == 1:
             print(datetime.now().strftime('%m-%d %H:%M:%S'), msg)
         elif level == 2:
-            print("\t", datetime.now().strftime('%m-%d %H:%M:%S'), msg)
+            # print("\t", datetime.now().strftime('%m-%d %H:%M:%S'), msg)
+            print("\t", datetime.now().strftime('%M:%S'), msg)
 
 
 # upbit exchange rate "USD/KRW"
@@ -101,7 +118,10 @@ def send_message(text):
     bot.sendMessage(telegram_chatid, text=text)
 
 def Autoplay():
-
+    message = 'trading 2 pair'
+    debug(message, 1)
+    send_message(message)
+    
     # exchanged fee(%)
     upbit_fee = 0.05
     binance_fee = 0
@@ -117,7 +137,7 @@ def Autoplay():
 def main():
     while True:
         Autoplay()
-        time.sleep(4)
+        time.sleep(60*2)
 
 if __name__ == "__main__":
     debug("START Wallet", 1)
@@ -126,5 +146,9 @@ if __name__ == "__main__":
     monitoring_kp = MonitoringKP()
     monitoring_kp.setDaemon(True)
     monitoring_kp.start()
+
+    monitoring_hg = MonitoringHedge()
+    monitoring_hg.setDaemon(True)
+    monitoring_hg.start()
 
     main()
